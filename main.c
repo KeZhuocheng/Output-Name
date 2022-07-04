ac7e34acd9abf0b631595e810a35c12fa2bc290c
KeZhuocheng <106873405+KeZhuocheng@users.noreply.github.com>
2022-07-04 19:44:59 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main()
+{
+
+    char n;
+    printf("Please write you name:");
+    scanf("%c",&n);
+    printf("Your name is:%c",n);
+    return 0;
+}
